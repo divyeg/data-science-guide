16a5dba8a9e53ab841a3e35174b7060cd1a0fbf5
Divye <divye@Divyes-MacBook-Air.local>
2023-08-06 09:18:17 -0700

    Added Bubble Sort Algorithm

--- python-datastructures/sorting/algorithms.py
+++ python-datastructures/sorting/algorithms.py
@@ -5,7 +5,7 @@ class SortingAlgos(object):
     def __init__(self):
         pass
 
-    def SelectionSort(A):
+    def SelectionSort(self, A):
         """
         This function is used to sort list of arrays using brute force sorting algorithm
 
@@ -21,4 +21,17 @@ class SortingAlgos(object):
             A[i], A[min_index] = A[min_index], A[i]
 
         logger.debug(f"Sorting Array is completed using brute force algorithm")
+        return A
+
+    def BubbleSort(self, A):
+        """
+        This function is used to sort list of arrays using bubble sort sorting algorithm
+
+        """
+
+        for i in range(len(A)):
+            for j in reversed(range(i + 1, len(A))):
+                if A[j - 1] > A[j]:
+                    A[j], A[j - 1] = A[j - 1], A[j]
+
         return A
