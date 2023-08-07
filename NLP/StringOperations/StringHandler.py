c09d519f8b8fe267adde7d59e58f6faa496447eb
Divye <42697627+divyeg@users.noreply.github.com>
2023-08-07 00:36:52 -0700

    Updates to string handler

--- NLP/StringOperations/StringHandler.py
+++ NLP/StringOperations/StringHandler.py
@@ -1,8 +1,9 @@
 from nltk.corpus import stopwords
+from collections import Counter
 import string
 
 
-class DataHandler(object):
+class StringHandler(object):
     def __init__(self):
         pass
 
@@ -39,5 +40,33 @@ class DataHandler(object):
         """
         df["text_wo_stopwords"] = df[text_column].apply(
             lambda text: self.remove_stop_words_from_text(text)
+        )
+        return df
+
+    def count_word_frequency_df(self, df, text_column):
+        """
+        This function is used to count word frequency from text column in pandas dataframe
+        """
+        count = Counter()
+        for text in df[text_column].values:
+            for word in text.split():
+                count[word] += 1
+        return count
+
+    def remove_topn_frequent_words_text(self, text, counter, n):
+        """
+        This function is used to remove most frequent words from text
+
+        """
+        frequent_words = set([w for (w, wc) in counter.most_common[n]])
+        return " ".join([word for word in text.split() if word not in frequent_words])
+
+    def remove_topn_frequent_words_df(self, df, text_column, counter, n):
+        """
+        This function is used to remove most frequent words from text column in pandas dataframe
+
+        """
+        df["text_wo_stopfreq"] = df[text_column].apply(
+            lambda text: self.remove_topn_frequent_words_text(text, counter, n)
         )
         return df
