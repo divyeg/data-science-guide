a81d023d77d3f59aaaac78567fba11b3bbb9a43f
Divye <42697627+divyeg@users.noreply.github.com>
2024-04-12 16:19:29 -0700

    Added Kmeans Clustering Module

--- PythonDatastructures/trees/algorithms.py
+++ PythonDatastructures/trees/algorithms.py
@@ -53,3 +53,68 @@ class TreeAlgos(object):
             prev.right = new_node
 
         return root
+
+    def DeleteNodeBST(self, root, value_list):
+        """
+        This function is used to delete list of nodes given root node
+        """
+        if root is None:
+            return None
+
+        for i in value_list:
+            curr = root
+            prev = None
+            while curr is not None:
+                if curr.value == i:
+                    break
+                elif curr.value < i:
+                    prev = curr
+                    curr = curr.right
+                else:
+                    prev = curr
+                    curr = curr.left
+
+            if curr is None:
+                continue
+
+            if curr.right is None and curr.left is None:  # current node is a leaf
+                if prev is None:
+                    root = None
+                elif curr is prev.left:
+                    prev.left = None
+                else:
+                    prev.right = None
+
+            elif curr.right is None and curr.left is not None:
+                if prev is None:
+                    root = curr.left
+                elif curr is prev.left:
+                    prev.left = curr.left
+                else:
+                    prev.right = curr.left
+
+            elif curr.right is not None and curr.left is None:
+                if prev is None:
+                    root = curr.right
+                elif curr is prev.left:
+                    prev.left = curr.right
+                else:
+                    prev.right = curr.right
+            else:
+                # find successor
+                succ = curr.right
+                curr_temp = curr.right
+                while curr_temp.left is not None:
+                    curr_temp = curr_temp.left
+                    succ = curr_temp
+
+                self.DeleteNodeBST(root, [succ.value])
+
+                if prev is None:
+                    root.value = succ.value
+                elif curr is prev.left:
+                    prev.left.value = succ.value
+                else:
+                    prev.right.value = succ.value
+
+        return root
